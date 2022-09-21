3b7d71fe85286c208e1804c0707c9d06ac908fcf
Hassanjkhan99 <Hassanjamilkhan114@gmail.com>
2022-09-21 09:05:25 -0700

    Comparison Operators

--- JS Operators.js
+++ JS Operators.js
@@ -21,3 +21,24 @@ z = b - a; //shows subtraction and returns 5
 z = a * b; //shows multiplication and returns 50
 z = b / a; //shows division and returns 2
 z = a % 2; //shows division remainder 1
+
+// Comparison Operators
+// ==	Compares the equality of two operands without considering type.
+// ===	Compares equality of two operands with type.
+// !=	Compares inequality of two operands.
+// >	Returns a boolean value true if the left-side value is greater than the right-side value; otherwise, returns false.
+// <	Returns a boolean value true if the left-side value is less than the right-side value; otherwise, returns false.
+// >=	Returns a boolean value true if the left-side value is greater than or equal to the right-side value; otherwise, returns false.
+// <=	Returns a boolean value true if the left-side value is less than or equal to the right-side value; otherwise, returns false
+
+var a = 5, b = 10, c = "5";
+var s = a;
+
+a == c; // returns true
+a === c; // returns false
+a == s; // returns true
+a != b; // returns true
+a > b; // returns false
+a < b; // returns true
+a >= b; // returns false
+a <= b; // returns true
