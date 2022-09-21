667d341d21ec413387283dc1e629f22299fd5f0f
Hassanjkhan99 <Hassanjamilkhan114@gmail.com>
2022-09-21 08:48:02 -0700

    JS Operators.js

---
+++ JS Operators.js
@@ -0,0 +1,5 @@
+// JavaScript includes operators same as other languages. An operator performs some operation on single or multiple operands (data value) and produces a result.
+
+let x = 5
+let y = 2
+let z = x + y
