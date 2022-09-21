f24496586dbf65277cd28dea127abf312afc255d
Hassanjkhan99 <Hassanjamilkhan114@gmail.com>
2022-09-21 08:38:20 -0700

    DataTypes.js

---
+++ DataTypes.js
@@ -0,0 +1,7 @@
+// In Javascript, there are five basic, or primitive, types of data. The five most basic types of data are strings, numbers, booleans, undefined, and null. We refer to these as primitive data types. A single variable can only store a single type of data
+
+let x = 10
+let y = "a,b"
+let z = true
+let c = undefined
+let d = null
