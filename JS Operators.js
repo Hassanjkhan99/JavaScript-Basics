ee94dd87976ba6992d987e42e1b7b25817c52492
Hassanjkhan99 <Hassanjamilkhan114@gmail.com>
2022-09-21 08:54:42 -0700

    assignment operator

---
+++ JS Operators.js
@@ -0,0 +1,8 @@
+// JavaScript includes operators same as other languages. An operator performs some operation on single or multiple operands (data value) and produces a result.
+
+//assignment operator ==> =
+let x = 5 // assigns number 5 to 'x'
+let y = 2
+
+
+let z = x + y
