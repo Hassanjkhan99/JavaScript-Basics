e98a9f37d0f7e4e17c50f41392d0096c9881c094
Hassanjkhan99 <Hassanjamilkhan114@gmail.com>
2022-09-21 08:24:33 -0700

    statement.js

---
+++ statements.js
@@ -0,0 +1,2 @@
+// JavaScript statements are used to control the flow of a script or a program. The statements are composed of values, expressions, operators, comments, and keywords. Ideally, any JavaScript code you write is made up of separate statements
+
