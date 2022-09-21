a87f152f085a73ed39b77f26bacd0ad9baa11c41
Hassanjkhan99 <Hassanjamilkhan114@gmail.com>
2022-09-21 09:01:28 -0700

    Arithmetic Operators

--- JS Operators.js
+++ JS Operators.js
@@ -3,6 +3,21 @@
 //assignment operator ==> =
 let x = 5 // assigns number 5 to 'x'
 let y = 2
-
-
 let z = x + y
+
+// Arithmetic Operators
+// +	Adds two numeric operands.
+// -	Subtract right operand from left operand
+// *	Multiply two numeric operands.
+// /	Divide left operand by right operand.
+// %	Modulus operator. Returns remainder of two operands.
+// ++	Increment operator. Increase operand value by one.
+// --	Decrement operator. Decrease value by one.
+
+var a = 5, b = 10;
+
+var c = a + b; //shows addition and returns 15
+z = b - a; //shows subtraction and returns 5
+z = a * b; //shows multiplication and returns 50
+z = b / a; //shows division and returns 2
+z = a % 2; //shows division remainder 1
