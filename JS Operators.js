03b5360376dde6d0102468e6a2eb255b34e0a3b5
Hassanjkhan99 <Hassanjamilkhan114@gmail.com>
2022-09-22 09:42:39 -0700

    JS Logical Operators

--- JS Operators.js
+++ JS Operators.js
@@ -42,3 +42,24 @@ a > b; // returns false
 a < b; // returns true
 a >= b; // returns false
 a <= b; // returns true
+
+// Logical Operators
+
+// In JavaScript, the logical operators are used to combine two or more conditions.
+
+// Operator	Description
+// &&	    && is known as AND operator. It checks whether two operands are non-zero or not (0, false, undefined, null or "" are considered as zero). It returns 1 if they are non-zero; otherwise, returns 0.
+// ||	    || is known as OR operator. It checks whether any one of the two operands is non-zero or not (0, false, undefined, null or "" is considered as zero). It returns 1 if any one of of them is non-zero; otherwise, returns 0.
+// !	    ! is known as NOT operator. It reverses the boolean result of the operand (or condition). !false returns true, and !true returns false.
+
+var a = 5, b = 10;
+
+(a != b) && (a < b); // returns true
+
+(a > b) || (a == b); // returns false
+
+(a < b) || (a == b); // returns true
+
+!(a < b); // returns false
+
+!(a > b); // returns true
